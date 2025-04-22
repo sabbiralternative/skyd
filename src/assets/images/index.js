c763160aea872529ab8f823be6865bd0b2cdf2c4
sabbirHossain <sabbirshnt@gmail.com>
2025-04-22 17:41:33 +0600

    done

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,5 @@
 import transparent from "/images/transparent.gif";
 import logo from "/images/skyexchange/logo.png";
+import telegram from "../img/telegram.png";
 
-export default { transparent, logo };
+export default { transparent, logo, telegram };
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/shared/Header/Header";
+import WhatsApp from "../components/shared/WhatsApp";
 
 const MainLayout = () => {
   return (
     <>
+      <WhatsApp />
       <Header />
       <Outlet />
     </>
